5e363e3e8abb32dfa011b5bdb5a8f297ed4108d0
David Wilke <david-wilke@live.com>
2025-03-10 22:38:31 +0100

    optimiz cloud intervall and spawn point function

--- classes/cloud.class.js
+++ classes/cloud.class.js
@@ -2,19 +2,27 @@ class Cloud extends MovableObject {
 
     height = 200;
     width = 550;
+    minXDistance = 500;
 
     constructor() {
         super().loadImage('./img/5_background/layers/4_clouds/1.png')
         this.x = 20 + Math.random() * 8000;
+        this.x = this.generateSpawnPointX(500, 2500);
         this.loadImage
         this.y = 20 + Math.random() * 100;
+        this.y = this.generateSpawnPointY(20, 150);
         this.speed = this.speed + Math.random() * 0.6;
         this.animate();
     }
 
     animate() {
-        setInterval(() => {
-            this.moveLeft();
+        let cloudInt = setInterval(() => {
+            if (this.x <= -100) {
+                window.clearInterval(cloudInt);
+            }
+            else {
+                this.moveLeft();
+            }
         }, 60);
     }
 }
