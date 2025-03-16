f44452e2ed942c8bf7eecd8c11f2d3a5e51e914c
David Wilke <david-wilke@live.com>
2025-03-16 13:01:31 +0100

    add new image stack

--- classes/bottle.class.js
+++ classes/bottle.class.js
@@ -18,10 +18,20 @@ class Bottle extends Reward {
         './img/6_salsa_bottle/2_salsa_bottle_on_ground.png'
     ];
 
+    IMG_BOTTLE_SPLASH = [
+        'img/6_salsa_bottle/bottle_rotation/bottle_splash/1_bottle_splash.png',
+        'img/6_salsa_bottle/bottle_rotation/bottle_splash/2_bottle_splash.png',
+        'img/6_salsa_bottle/bottle_rotation/bottle_splash/3_bottle_splash.png',
+        'img/6_salsa_bottle/bottle_rotation/bottle_splash/4_bottle_splash.png',
+        'img/6_salsa_bottle/bottle_rotation/bottle_splash/5_bottle_splash.png',
+        'img/6_salsa_bottle/bottle_rotation/bottle_splash/6_bottle_splash.png'
+    ]
+
     constructor(x, y) {
         super();
         this.loadImage(this.IMG_BOTTLE[0]);
         this.loadImages(this.IMG_BOTTLE);
+        this.loadImages(this.IMG_BOTTLE_SPLASH);
         this.x = x !== undefined ? x : this.generateSpawnPointX(0, 1800);
         this.y = y !== undefined ? y : this.generateSpawnPointY(10, 350);
         // console.log(this.x);
